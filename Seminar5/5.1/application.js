1e65683df6b136f2fcac5ffc87a281eba70ade0c
BiancaAlle <bianca.alexia.vlad@gmail.com>
2025-11-11 18:13:11 +0000

    Seminar 5 prima parte

---
+++ Seminar5/5.1/application.js
@@ -0,0 +1,5 @@
+//Exercitiu
+import { secondsPerMinute, minutesToSeconds } from "./time.js";
+
+console.log("1 minut has", secondsPerMinute, "seconds");
+console.log("5 minutes have =", minutesToSeconds(5), "seconds");
---
+++ Seminar5/5.1/time.js
@@ -0,0 +1,6 @@
+//Exercitiu
+export const secondsPerMinute = 60;
+
+export function minutesToSeconds(minutes) {
+  return minutes * secondsPerMinute;
+}
