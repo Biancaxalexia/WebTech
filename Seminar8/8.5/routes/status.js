6dbf0d81e362f8645e149a95a3b111ddb02a74a3
BiancaAlle <bianca.alexia.vlad@gmail.com>
2025-11-29 23:33:54 +0000

    Seminar8

---
+++ Seminar8/8.5/routes/status.js
@@ -0,0 +1,9 @@
+//Exemplu
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ Status: "OK" });
+});
+
+module.exports = router;
---
+++ Seminar8/8.6/index.js
@@ -0,0 +1,43 @@
+//Exemplu + exercitiu
+"use strict";
+
+const express = require("express");
+const departmentsRouter = require("./routes/departments");
+const statusRouter = require("./routes/status");
+//require("dotenv").config();
+
+const app = express();
+
+/*const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};*/
+
+//app.use(logger);
+
+// app.use((err, req, res, next) => {
+//     console.log(err.stack)
+//     res.status(500).json({ Error: "ERR" })
+// })
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ error: "Something broke!" });
+});
+
+const middleware = (req, res, next) => {
+  console.log(`method: ${req.method}\nurl: ${req.url}`);
+  next();
+};
+
+app.use(middleware);
+
+app.use("/api", departmentsRouter);
+app.use("/status", statusRouter);
+
+//app.set("port", process.env.PORT || 7000);
+app.set("port", 7000);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server started on http://localhost:${app.get("port")}`);
+});
